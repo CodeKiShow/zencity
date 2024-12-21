7d4cb1ae2c721d7fbccd2da35a4247082476f325
CodeKiShow <135666146+CodeKiShow@users.noreply.github.com>
2024-12-22 00:15:16 +0100

    Add components index file for easier imports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as InteractiveMap } from './InteractiveMap';
+export { default as GeoFavorites } from './GeoFavorites';
+export { default as TransportInfo } from './TransportInfo';
+export { default as LocationTracker } from './LocationTracker';
+export { default as RoutePlanner } from './RoutePlanner';
