ebc1b7e8868be280018c5c323ee0fca88b7873b0
CodeKiShow <135666146+CodeKiShow@users.noreply.github.com>
2024-12-22 01:32:23 +0100

    Add styling configurations

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'zen-green': '#34D399',
+        'zen-blue': '#60A5FA'
+      }
+    }
+  },
+  plugins: []
+};
